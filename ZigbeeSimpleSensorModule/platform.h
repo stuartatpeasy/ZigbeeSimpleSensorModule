a1e221e33045f764f6f474857a90e1a82e57180c
Stuart Wallace <stuartw@atom.net>
2018-09-16 18:36:03 +0100

    Moved uC-specific pin definitions from platform.h -> platform_attinyX16.h

--- ZigbeeSimpleSensorModule/platform.h
+++ ZigbeeSimpleSensorModule/platform.h
@@ -4,7 +4,11 @@
     platform.h: declarations relevant to both main.c and other modules
 
     Stuart Wallace <stuartw@atom.net>, July 2018.
+
+    Note that these declarations make use of the GPIO*() macros defined in lib/gpio.h.
 */
+
+#include "platform_attinyX16.h"
 
 #define F_CPU       16000000UL
 
@@ -17,29 +21,16 @@
 #define PIN_BUTTON                  GPIOA(6)    // [I] Pushbutton
 // (unused)                         GPIOA(5)
 #define PIN_SENSOR_nENABLE          GPIOA(4)    // [O] Enable signal for sensors
-#define PIN_SPI_nSS_DEFAULT         GPIOA(4)    // [O] SPI slave select (default pin-set)
 #define PIN_AIN_LIGHT               GPIOA(3)    // [I] Light sensor analogue input
-#define PIN_SPI_SCK_DEFAULT         GPIOA(3)    // [O] SPI clock (default pin-set)
 #define PIN_AIN_TEMP                GPIOA(2)    // [I] Temperature sensor analogue input
-#define PIN_SPI_MISO_DEFAULT        GPIOA(2)    // [I] SPI MISO (default pin-set)
-#define PIN_USART_RXD_ALT           GPIOA(2)    // [I] USART RXD (alternate pin-set)
 #define PIN_AIN_VBATT               GPIOA(1)    // [I] Battery voltage sensor analogue input
-#define PIN_SPI_MOSI_DEFAULT        GPIOA(1)    // [O] SPI MOSI (default pin-set)
-#define PIN_USART_TXD_ALT           GPIOA(1)    // [O] USART TXD (alternate pin-set)
 // (unused - nRESET/UPDI)           GPIOA(0)
 
 // Port B
 #define PIN_XBEE_SPI_nATTN          GPIOB(5)    // [I] XBee SPI nATTN (attention request) signal
 #define PIN_XBEE_NRESET             GPIOB(4)    // [O] XBee nRESET
-#define PIN_USART_RXD_DEFAULT       GPIOB(3)    // [I] USART RXD (default pin-set)
-#define PIN_USART_TXD_DEFAULT       GPIOB(2)    // [O] USART TXD (default pin-set)
 #define PIN_XBEE_ON_nSLEEP          GPIOB(1)    // [I] XBee ON/nSLEEP (awake/asleep) indicator
 #define PIN_XBEE_SLEEP_RQ           GPIOB(0)    // [O] XBee sleep request
 
-// Port C
-#define PIN_SPI_nSS_ALT             GPIOC(3)    // [O] SPI slave select (alternate pin-set)
-#define PIN_SPI_MOSI_ALT            GPIOC(2)    // [O] SPI MOSI (alternate pin-set)
-#define PIN_SPI_MISO_ALT            GPIOC(1)    // [I] SPI MISO (alternate pin-set)
-#define PIN_SPI_SCK_ALT             GPIOC(0)    // [O] SPI clock (alternate pin-set)
-
 #endif
+
